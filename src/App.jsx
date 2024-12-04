d95e3997d4f54327f8392d5e862b28367526755c
Nabeel Abrar <nabeelabrar9099@gmail.com>
2024-12-04 06:53:13 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <h1 className='text-red-500'>Habiba Nabeel</h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/signin' element={<Signin />} />
+    <Route path='/signup' element={<Signup />} />
+    <Route path='/profile' element={<Profile />}/>
+    <Route path='/about' element={<About />} />
+   </Routes>
+   </BrowserRouter>
       
     
   )
