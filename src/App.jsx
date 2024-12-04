e31c8320894a74daec9d67cff67586ddb87e3170
Nabeel Abrar <nabeelabrar9099@gmail.com>
2024-12-05 03:58:14 +0500

    Header and Link Redirect

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Header />
    <Routes>
+    
     <Route path='/' element={<Home />} />
     <Route path='/signin' element={<Signin />} />
     <Route path='/signup' element={<Signup />} />
